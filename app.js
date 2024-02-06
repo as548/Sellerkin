4e04d1ad5efc794d822a906afcb988d31071fd79
as548 <113757091+as548@users.noreply.github.com>
2024-02-06 19:03:16 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,12 +5,20 @@ const router = require("./routes/router");
 const cors = require("cors")
 const port = 4000;
 
-
+const path =require("path")
+if(process.env.NODE_ENV!=="PRODUCTION"){
+   require("dotenv").config({./.env"});
+}
 // middle ware
 app.use(express.json());
 app.use(cors());
 app.use(router)
 
+app.use(express.static(path.join(__dirname,"./frotend/build")))
+
+app.get("*",(req,res)=>{
+   res.sendFile(path.resolve(__dirname,"./frotend/build/index.html"))
+});    
 
 app.listen(port,()=>{
     console.log(`server start at port no :${port}`)
